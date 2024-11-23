50b5803270003a02ac030e82eaff58e4b698d4f4
DatZeroSize <152140093+DatZeroSize@users.noreply.github.com>
2024-11-23 22:31:39 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { Auth0Provider } from '@auth0/auth0-react'; // Thêm Auth0Provider
 import App from './App';
 import './index.css';
 
-const domain = "dev-i3z1n8irk4h33u3v.us.auth0.com"; // Thay bằng domain từ Auth0
-const clientId = "SLd6VW8jvkazouRYM8hriuhi7YnnRm5O"; // Thay bằng clientId từ Auth0
+const domain = "dev-krykox1jnhghdidl.us.auth0.com"; // Thay bằng domain từ Auth0
+const clientId = "1F3M1rAcwZGBMP1rOYsvPd4uYrLNf5H5"; // Thay bằng clientId từ Auth0
 
 ReactDOM.render(
   <Auth0Provider
