22849dc8842439e2b94b6e9be42c04be23c24904
DatZeroSize <152140093+DatZeroSize@users.noreply.github.com>
2024-11-23 22:30:35 +0700

    Update auth0Config.js

--- src/auth0Config.js
+++ src/auth0Config.js
@@ -1,7 +1,5 @@
-// src/auth0Config.js
 export const auth0Config = {
-    domain: "dev-i3z1n8irk4h33u3v.us.auth0.com", // Thay bằng giá trị Domain từ Auth0
-    clientId: "SLd6VW8jvkazouRYM8hriuhi7YnnRm5O", // Thay bằng giá trị Client ID từ Auth0
-    redirectUri: window.location.origin,
-  };
-  
+  domain: "dev-krykox1jnhghdidl.us.auth0.com", // Domain của bạn
+  clientId: "1F3M1rAcwZGBMP1rOYsvPd4uYrLNf5H5", // Client ID từ Auth0
+  redirectUri: "https://online-dictionary-frontend.onrender.com", // Đường dẫn đúng với callback
+};
