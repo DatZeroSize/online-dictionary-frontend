f90a41823529edcafdb8a10093dd24df27411096
DatZeroSize <152140093+DatZeroSize@users.noreply.github.com>
2024-11-25 20:28:17 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import './index.css';
 
 const domain = "dev-krykox1jnhghdidl.us.auth0.com"; // Thay bằng domain từ Auth0
 const clientId = "1F3M1rAcwZGBMP1rOYsvPd4uYrLNf5H5"; // Thay bằng clientId từ Auth0
+const audience = "https://dictionary-backend-ocuq.onrender.com"; // URL API backend của bạn
+const redirectUri = "https://online-dictionary-frontend.onrender.com"; // URL ứng dụng frontend của bạn
 
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
     authorizationParams={{
-      redirect_uri: window.location.origin,
+      redirect_uri: redirectUri, // Đảm bảo đây là URL chính xác
+      audience: audience, // URL API bạn muốn kết nối
+      scope: "read:data write:data", // Các quyền API bạn cần
     }}
   >
     <App />
